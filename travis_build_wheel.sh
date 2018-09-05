3af8747567b729397148e6f9eb532dca97c89799
Manuel Barkhau <mbarkhau@gmail.com>
2018-09-05 22:10:16 +0200

    travis experiment #14\n\ndon't install new interpreter when on py36+

--- travis_build_wheel.sh
+++ travis_build_wheel.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 set -e
 
-if [[ $(which python3.6) ]]; then
-    python3.6 --version
+
+if [[ $(python -c "import sys;sys.exit(sys.version[:3] < '3.6')")  ]]; then
+    python --version
+    python -m pip install setuptools lib3to6
+    python setup.py bdist_wheel --python-tag=py2.py3
 else
     curl -sSf -o python-3.6.tar.bz2 https://s3.amazonaws.com/travis-python-archives/binaries/ubuntu/16.04/x86_64/python-3.6.tar.bz2
     tar xjf python-3.6.tar.bz2 --directory /
     source $HOME/virtualenv/python3.6.0/bin/activate
+
+    python3.6 --version
+    python3.6 -m pip install setuptools lib3to6
+    python3.6 setup.py bdist_wheel --python-tag=py2.py3
 fi
 
-python3.6 --version
-python3.6 -m pip install setuptools lib3to6
-python3.6 setup.py bdist_wheel --python-tag=py2.py3
