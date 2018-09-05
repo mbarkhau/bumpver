225998de0f130260ef88ab508c3db083b4060afe
Manuel Barkhau <mbarkhau@gmail.com>
2018-09-05 20:33:39 +0200

    travis experiment #10\n\nin retrospect it couldn't have worked...

--- travis_run_tests.sh
+++ travis_run_tests.sh
@@ -7,15 +7,17 @@ rst2html5 --strict CHANGELOG.rst > /dev/null
 # first test module from installed wheel
 python -m pip install $(ls -1t dist/pycalver*.whl | head -n 1)
 grep 'coding: utf-8' $(python -c 'import pycalver;print(pycalver.__file__)')
-python -m pytest --cov=pycalver test/
+python -m pip install pytest
+python -m pytest test/
 
 if [[ $(python -c "import sys;print(sys.version[:3])") == "3.7" ]]; then
+  python -m pip install $(cat requirements-test.txt)
   python -m flake8 src/pycalver/
 
   MYPYPATH=stubs/ python -m mypy src/pycalver/;
 
   # next test module from src/
   PYTHONPATH=src/:$PYTHONPATH python -m pytest --cov=pycalver test/;
+  codecov
 fi
 
-codecov
