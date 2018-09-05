e9642ac3d9cfd632714124ed011bfecd5b8a46ac
Manuel Barkhau <mbarkhau@gmail.com>
2018-09-05 09:57:52 +0200

    travis experiment #7

--- travis_build_wheel.sh
+++ travis_build_wheel.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-if [ $(which python3.7) ]; then
+set -e
+
+if [[ $(which python3.7) ]]; then
     python3.7 --version
 else
     curl -sSf -o python-3.7.tar.bz2 https://s3.amazonaws.com/travis-python-archives/binaries/ubuntu/16.04/x86_64/python-3.7.tar.bz2
---
+++ travis_run_tests.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+rst2html5 --strict README.rst > /dev/null
+rst2html5 --strict CHANGELOG.rst > /dev/null
+
+# first test module from installed wheel
+python -m pip install $(ls -1t dist/pycalver*.whl | head -n 1)
+grep 'coding: utf-8' $(python -c 'import pycalver;print(pycalver.__file__)')
+python -m pytest --cov=pycalver test/
+
+if [[ $(python -c "import sys;print(sys.version[:3])") == "3.7" ]]; then
+  python -m flake8 src/pycalver/
+
+  MYPYPATH=stubs/
+  python -m mypy src/pycalver/
+
+  # next test module from src/
+  PYTHONPATH=src/:$PYTHONPATH
+  python -m pytest --cov=pycalver test/
+fi
+
+codecov
