9422e7b8ba2312c1d5f817a74b717d752268912a
Manuel Barkhau <mbarkhau@gmail.com>
2018-12-22 01:10:48 +0100

    release to fix badge in readme

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setuptools.setup(
     author="Manuel Barkhau",
     author_email="mbarkhau@gmail.com",
     url="https://gitlab.com/mbarkhau/pycalver",
-    version="201812.16",
+    version="201812.16post1",
     keywords="version versioning bumpversion calver",
     description="CalVer versioning for python libraries.",
     long_description=long_description,
