1b8c325d832b23f39431cc34273e5dbb09af889f
Manuel Barkhau <mbarkhau@gmail.com>
2019-07-08 15:39:04 +0000

    Don't fail when ENABLE_BACKTRACE=1 but backtrace is not available.

--- src/pycalver/cli.py
+++ src/pycalver/cli.py
@@ -28,9 +28,12 @@ _VERBOSE = 0
 # To enable pretty tracebacks:
 #   echo "export ENABLE_BACKTRACE=1;" >> ~/.bashrc
 if os.environ.get('ENABLE_BACKTRACE') == '1':
-    import backtrace
+    try
+        import backtrace
 
-    backtrace.hook(align=True, strip_path=True, enable_on_envvar_only=True)
+        backtrace.hook(align=True, strip_path=True, enable_on_envvar_only=True)
+    except ImportError:
+        pass
 
 
 click.disable_unicode_literals_warning = True
