173593c8f72da335c0127eff9eee5b8a5835e708
Manuel Barkhau <mbarkhau@gmail.com>
2020-10-05 19:33:40 +0000

    fix for renamed module

--- setup.py
+++ setup.py
@@ -73,7 +73,7 @@ setuptools.setup(
     install_requires=install_requires,
     entry_points="""
         [console_scripts]
-        pycalver=pycalver.__main__:cli
+        pycalver=pycalver.cli:cli
     """,
     python_requires=">=2.7",
     zip_safe=True,
