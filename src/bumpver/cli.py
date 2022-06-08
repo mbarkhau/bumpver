0d8b6a55386f03da1f75822cbfbd8614000d60fd
Manuel Barkhau <mbarkhau@gmail.com>
2022-06-08 19:24:45 +0000

    make fmt

--- src/bumpver/cli.py
+++ src/bumpver/cli.py
@@ -246,7 +246,7 @@ def version_options(function: typ.Callable) -> typ.Callable:
     return decorated
 
 
-@click.group(context_settings={"help_option_names": ["-h", "--help"]})
+@click.group(context_settings={'help_option_names': ["-h", "--help"]})
 @click.version_option(version="2022.1116")
 @verbose_option
 def cli(verbose: int = 0) -> None:
