b740ec7f0b5515fabc434bd822881c26e8a95279
Manuel Barkhau <mbarkhau@gmail.com>
2023-10-13 10:55:06 +0000

    bump 2023.1127 -> 2023.1128

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ setuptools.setup(
     author="Manuel Barkhau",
     author_email="mbarkhau@gmail.com",
     url="https://github.com/mbarkhau/bumpver",
-    version="2023.1127",
+    version="2023.1128",
     keywords="version bumpver calver semver versioning bumpversion pep440",
     description="Bump version numbers in project files.",
     long_description=long_description,
